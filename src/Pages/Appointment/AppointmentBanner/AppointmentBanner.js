1db48c11a36302b92046a538ef072c00e8393cf6
Mezan2002 <mezanurr009@gmail.com>
2022-11-13 19:20:08 +0600

    Appointment banner calender added

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import chair from "../../../assets/images/chair.png";
+import { DayPicker } from "react-day-picker";
+import BgImage from "../../../assets/images/bg.png";
+import { format } from "date-fns";
 
 const AppointmentBanner = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
-    <header>
+    <header
+      className="mt-60 mb-20"
+      style={{ backgroundImage: `url(${BgImage})` }}
+    >
       <div className="hero">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="lg:w-1/2">
             <img
               src={chair}
-              className="max-w-sm rounded-lg shadow-2xl"
+              className="rounded-lg shadow-2xl"
               alt="dentist chair"
             />
           </div>
           <div className="lg:w-1/2">
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+            <DayPicker
+              mode="single"
+              selected={selectedDate}
+              onSelect={setSelectedDate}
+            />
+            <p>You Have Selected Date: {format(selectedDate, "PPPP")}</p>
           </div>
         </div>
       </div>
